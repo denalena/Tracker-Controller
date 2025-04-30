924f0dca572b530b29ec5c52756a8881b8c2f264
dena <git@denalena.de>
2025-04-30 21:35:43 +0200

    debouncing + extras

--- Tracker_Controller.ino
+++ Tracker_Controller.ino
@@ -1,21 +1,25 @@
-/*Tracker Controller  for ProMicro.  Charles Hobbs 2023
+/*Tracker Controller  for ProMicro.  Charles Hobbs 2023, dena 2025
 Emmulates either joystick or keyboard.  Multiple alternate keymapping modes available, 
 configured by holding a key during start up.
 */
 #include <Keyboard.h>
-#include <Joystick.h>
+#include <Joystick.h> // https://github.com/MHeironimus/ArduinoJoystickLibrary
+#include "Timer.h"
 
+Timer timers[8];
 Joystick_ Joystick;
 
+const int DEBOUNCE = 50;
+
 //pin mapping
-byte a = 2;
-byte b = 3;
-byte up = 4;
-byte left = 5;
-byte right = 6;
-byte down = 7;
-byte star = 8;
-byte select = 9;
+byte a     = 2;  // m8:edit
+byte b     = 9;  // m8:option
+byte up    = 8;
+byte left  = 4;
+byte right = 3;
+byte down  = 6;   
+byte star  = 5;  // m8:play
+byte select = 7; // m8:shift
 
 int button[8] = {a, b, up, left, right, down, star, select};
 
@@ -29,7 +33,7 @@ int command[3][8] = {{1, 0, 12, 14, 15, 13, 9, 8},              //mode 0   gener
 
 //joystick or keyboard?   joystick = 1, keyboard = 0    {mode 0, mode 1, mode 2...}
 bool joyMode[3] = {1, 0, 0};    
-int mode = 0;  //default mode
+int mode = 1;  // default mode m8c
 
 int buttonState[8] = {};
 int previousButtonState[8] = {HIGH};
@@ -41,43 +45,43 @@ void setup() {
 
   for (int i = 0; i < 8; i++) {
     pinMode(button[i], INPUT_PULLUP);
+    timers[i].start();
   }
 
   // set mode
   if (digitalRead(up) == LOW) {
-    mode = 1;
+    mode = 0;
   }
   if (digitalRead(down) == LOW) {
     mode = 2;
   }
-/*  if (digitalRead(left) == LOW) {      
-    mode = 3;
-  }
-*/
 }
-void loop() {
 
+void loop() {
   for (int i = 0; i < 8; i++) {
     buttonState[i] = digitalRead(button[i]);
 
     if (buttonState[i] == LOW && previousButtonState[i] == HIGH) {
+      if (timers[i].read() > DEBOUNCE) {
+        timers[i].stop();
+        timers[i].start();
 
-      if (joyMode[mode] == 0) {
-        Keyboard.press(command[mode][i]);
+        if (joyMode[mode] == 0) {
+          Keyboard.press(command[mode][i]);
+        } else {
+          Joystick.setButton(command[mode][i], HIGH);
+        }
       }
-      else
-        Joystick.setButton(command[mode][i], HIGH);
-
     }
 
     if (buttonState[i] == HIGH && previousButtonState[i] == LOW) {
       if (joyMode[mode] == 0) {
         Keyboard.release(command[mode][i]);
-      }
-      else
+      } else {
         Joystick.setButton(command[mode][i], LOW);
+      }
     }
-    previousButtonState[i] = buttonState[i];
 
+    previousButtonState[i] = buttonState[i];
   }
 }
